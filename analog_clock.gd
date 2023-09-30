84fc144692aba9eadda96fc2df35c0808d574d12
SeukWon Kang <kasworld@gmail.com>
2023-09-30 20:59:32 +0900

    fix hour hand

--- analog_clock.gd
+++ analog_clock.gd
@@ -18,7 +18,6 @@ func new_minute_clock_face(rad :float)->ColorRect:
 	cr.pivot_offset = Vector2(w/2,540)
 	return cr
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for i in range(0,12):
@@ -29,18 +28,15 @@ func _ready() -> void:
 		var cr = new_minute_clock_face(minute2degree(i))
 		$Panel.add_child(cr)
 
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
 
 func _on_timer_timeout() -> void:
 	var timeNowDict = Time.get_datetime_dict_from_system()
 	$Panel/SecondHand.rotation = second2degree(timeNowDict["second"])
 	$Panel/MinuteHand.rotation = minute2degree(timeNowDict["minute"]) + second2degree(timeNowDict["second"]) / 60
-	$Panel/HourHand.rotation = hour2degree(timeNowDict["hour"]) + minute2degree(timeNowDict["minute"]) /60
+	$Panel/HourHand.rotation = hour2degree(timeNowDict["hour"]) + minute2degree(timeNowDict["minute"]) /12
 
 func second2degree(sec)->float:
 	return 2.0*PI/60.0*sec
