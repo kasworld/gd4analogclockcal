0a6ccc8472022fc1c34fd91e8e0629c32c4dda86
SeukWon Kang <kasworld@gmail.com>
2023-10-04 12:29:02 +0900

    size rate clock_R

--- analog_clock.gd
+++ analog_clock.gd
@@ -1,12 +1,48 @@
 extends Node2D
 
+#var vp_size :Vector2
+var clock_R : float
+var HourHand :ColorRect
+var HourHand2 :ColorRect
+var MinuteHand :ColorRect
+var SecondHand :ColorRect
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var vp_size = get_viewport_rect().size
+	clock_R = vp_size.y / 2
+
+	for i in range(0,60):
+		if i == 0:
+			add_child(new_clock_face(Color.WHITE, minute2rad(i), clock_R/40, clock_R/10))
+			add_child(new_clock_face(Color.RED, minute2rad(i), clock_R/100, clock_R/10))
+		elif i % 15 ==0:
+			add_child(new_clock_face(Color.WHITE, minute2rad(i), clock_R/50, clock_R/10))
+			add_child(new_clock_face(Color.MAGENTA, minute2rad(i), clock_R/200, clock_R/10))
+		elif i % 5 == 0 :
+			add_child(new_clock_face(Color.WHITE, minute2rad(i), clock_R/150, clock_R/10))
+		else :
+			add_child(new_clock_face(Color.WHITE, minute2rad(i), clock_R/200, clock_R/20))
+
+	HourHand = clock_hand(Global.HandDict.hour)
+	add_child(HourHand)
+	HourHand2 = clock_hand(Global.HandDict.hour2)
+	add_child(HourHand2)
+	MinuteHand = clock_hand(Global.HandDict.minute)
+	add_child(MinuteHand)
+	SecondHand = clock_hand(Global.HandDict.second)
+	add_child(SecondHand)
+
+	add_child(new_clock_center(Color.GRAY, PI/4, clock_R/15))
+	add_child(new_clock_center(Color.DIM_GRAY, PI/4, clock_R/20))
+
 func new_clock_face(co :Color, rad :float, w , l )->ColorRect:
 	var cr = ColorRect.new()
 	cr.color = co
 	cr.size = Vector2(w, l)
-	cr.position = Vector2(540-w/2, 0) - Vector2(540,540)
+	cr.position = Vector2(clock_R-w/2, 0) - Vector2(clock_R,clock_R)
 	cr.rotation = rad
-	cr.pivot_offset = Vector2(w/2,540)
+	cr.pivot_offset = Vector2(w/2,clock_R)
 	return cr
 
 func clock_hand(dict :Dictionary)->ColorRect:
@@ -25,41 +61,8 @@ func new_clock_center(co :Color, rad :float, r )->ColorRect:
 	cr.size = Vector2(r, r)
 	cr.position = Vector2(0, 0) - Vector2(r/2,r/2)
 	cr.rotation = rad
-	cr.pivot_offset = Vector2(r/2,r/2) #- Vector2(540,540)
+	cr.pivot_offset = Vector2(r/2,r/2)
 	return cr
-
-
-var HourHand :ColorRect
-var HourHand2 :ColorRect
-var MinuteHand :ColorRect
-var SecondHand :ColorRect
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-
-	for i in range(0,60):
-		if i == 0:
-			add_child(new_clock_face(Color.WHITE, minute2rad(i), 14, 50))
-			add_child(new_clock_face(Color.RED, minute2rad(i), 4, 50))
-		elif i % 15 ==0:
-			add_child(new_clock_face(Color.WHITE, minute2rad(i), 10, 50))
-			add_child(new_clock_face(Color.MAGENTA, minute2rad(i), 2, 50))
-		elif i % 5 == 0 :
-			add_child(new_clock_face(Color.WHITE, minute2rad(i), 6, 50))
-		else :
-			add_child(new_clock_face(Color.WHITE, minute2rad(i), 4, 20))
-
-	HourHand = clock_hand(Global.HandDict.hour)
-	add_child(HourHand)
-	HourHand2 = clock_hand(Global.HandDict.hour2)
-	add_child(HourHand2)
-	MinuteHand = clock_hand(Global.HandDict.minute)
-	add_child(MinuteHand)
-	SecondHand = clock_hand(Global.HandDict.second)
-	add_child(SecondHand)
-
-	add_child(new_clock_center(Color.GRAY, PI/4, 30))
-	add_child(new_clock_center(Color.DIM_GRAY, PI/4, 20))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
