3d079815004d3872a198753b0542d33a499c25b9
SeukWon Kang <kasworld@gmail.com>
2023-10-14 14:35:15 +0900

    use mouse accel to rotate

--- main.gd
+++ main.gd
@@ -5,13 +5,10 @@ var vp_size :Vector2
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	vp_size = get_viewport_rect().size
-
 	var calw = vp_size.x-vp_size.y
 	$Calendar.position = Vector2(vp_size.y+calw/2, vp_size.y /2 )
-
 	$DateLabel.position = Vector2(vp_size.y /2, vp_size.y/2 )
 	$TimeLabel.position = Vector2(vp_size.y /2, vp_size.y/2 )
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 var oldvt = Vector2(0,-100)
@@ -20,8 +17,15 @@ func _process(_delta: float) -> void:
 	if  vt != Vector3.ZERO :
 		oldvt = (Vector2(vt.x,vt.y) + oldvt).normalized() *100
 		var rad = oldvt.angle_to(Vector2(0,-1))
-#		oldrad = (rad + oldrad) /2
-		$AnalogClock.rotation = rad
-		$TimeLabel.rotation = rad
-		$DateLabel.rotation = rad
-		$Calendar.rotation = rad
+		rotate_all(rad)
+	else :
+		vt = Input.get_last_mouse_velocity()/100
+		oldvt = (Vector2(vt.x,vt.y) + oldvt).normalized() *100
+		var rad = oldvt.angle_to(Vector2(0,-1))
+		rotate_all(rad)
+
+func rotate_all(rad :float):
+	$AnalogClock.rotation = rad
+	$TimeLabel.rotation = rad
+	$DateLabel.rotation = rad
+	$Calendar.rotation = rad
