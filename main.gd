06a1c4d7e8a6cbbf46addf7feab6a3d111faf53c
SeukWon Kang <kasworld@gmail.com>
2023-10-18 11:08:12 +0900

    esc to exit

--- main.gd
+++ main.gd
@@ -31,3 +31,9 @@ func rotate_all(rad :float):
 	$TimeLabel.rotation = rad
 	$DateLabel.rotation = rad
 	$Calendar.rotation = rad
+
+# esc to exit
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_ESCAPE:
+			get_tree().quit()
