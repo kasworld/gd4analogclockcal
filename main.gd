4ecd292528b70c846fa9c46242a7211ae12940fc
SeukWon Kang <kasworld@gmail.com>
2023-11-16 17:45:14 +0900

    add animove

--- main.gd
+++ main.gd
@@ -2,37 +2,79 @@ extends Node2D
 
 var vp_rect :Rect2
 
+var calendar_pos_list :Array
+var timelabel_pos_list :Array
+var analogclock_pos_list :Array
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	set_color_mode_by_time()
 
-	init_http()
+#	init_http()
 	vp_rect = get_viewport_rect()
 
 	var calw = vp_rect.size.x-vp_rect.size.y
 	if calw > vp_rect.size.x /2 :
 		calw = vp_rect.size.y
 	$Calendar.init( Rect2(-calw/2, -calw/2, calw, calw) )
-	$Calendar.position = Vector2(vp_rect.size.x-calw/2, vp_rect.size.y/2 )
+	calendar_pos_list.append(Vector2(vp_rect.size.x-calw/2, vp_rect.size.y/2 ))
+	calendar_pos_list.append(Vector2(calw/2, vp_rect.size.y/2 ))
+	$Calendar.position = calendar_pos_list[0]
 
 	var co :Color
 	co = Global.colors.timelabel
 	$TimeLabel.init(
 		Rect2(-vp_rect.size.x/3/2, -vp_rect.size.y/4, vp_rect.size.x/3, vp_rect.size.y/6),
 		co, Global.make_shadow_color(co))
-	$TimeLabel.position = Vector2(vp_rect.size.y/2, vp_rect.size.y/2 )
+	timelabel_pos_list.append(Vector2(vp_rect.size.y/2, vp_rect.size.y/2 ))
+	timelabel_pos_list.append(Vector2(vp_rect.size.x - vp_rect.size.y/2, vp_rect.size.y/2 ))
+	$TimeLabel.position = timelabel_pos_list[0]
 
 	$AnalogClock.init( Vector2(0, 0), vp_rect.size.y/2 , 9.0 )
-	$AnalogClock.position = Vector2(vp_rect.size.y/2, vp_rect.size.y/2 )
+	analogclock_pos_list.append(Vector2(vp_rect.size.y/2, vp_rect.size.y/2 ))
+	analogclock_pos_list.append(Vector2(vp_rect.size.x - vp_rect.size.y/2, vp_rect.size.y/2 ))
+	$AnalogClock.position = analogclock_pos_list[0]
 
 	var msgrect = Rect2( vp_rect.size.x * 0.2 ,vp_rect.size.y * 0.4 , vp_rect.size.x * 0.6 , vp_rect.size.y * 0.2   )
-	$TimedMessage.init(msgrect, tr("gd4analogclockcal 2.0.2"))
+	$TimedMessage.init(msgrect, tr("gd4analogclockcal 2.1.0"))
 	$TimedMessage.show_message("Copyright 2023 SeukWon Kang (kasworld@gmail.com)")
 
 
+func reset_pos()->void:
+	$TimeLabel.position = timelabel_pos_list[0]
+	$Calendar.position = calendar_pos_list[0]
+	$AnalogClock.position = analogclock_pos_list[0]
+	$AniMove.stop()
+
+func animove_toggle()->void:
+	$AniMove.toggle()
+	if not $AniMove.enabled:
+		reset_pos()
+
+func animove_step():
+	if not $AniMove.enabled:
+		return
+	var ms = $AniMove.get_ms()
+	match $AniMove.state%2:
+		0:
+			$AniMove.move_by_ms($TimeLabel, timelabel_pos_list[0], timelabel_pos_list[1], ms)
+			$AniMove.move_by_ms($Calendar, calendar_pos_list[0], calendar_pos_list[1], ms)
+			$AniMove.move_by_ms($AnalogClock, analogclock_pos_list[0], analogclock_pos_list[1], ms)
+		1:
+			$AniMove.move_by_ms($TimeLabel, timelabel_pos_list[1], timelabel_pos_list[0], ms)
+			$AniMove.move_by_ms($Calendar, calendar_pos_list[1], calendar_pos_list[0], ms)
+			$AniMove.move_by_ms($AnalogClock, analogclock_pos_list[1], analogclock_pos_list[0], ms)
+		_:
+			print_debug("invalid state", $AniMove.state)
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-var oldvt = Vector2(0,-100)
 func _process(_delta: float) -> void:
+	animove_step()
+	rot_by_accel()
+
+var oldvt = Vector2(0,-100)
+func rot_by_accel()->void:
 	var vt = Input.get_accelerometer()
 	if  vt != Vector3.ZERO :
 		oldvt = (Vector2(vt.x,vt.y) + oldvt).normalized() *100
@@ -74,7 +116,6 @@ func init_http():
 func helloed():
 	print_debug("helloed")
 
-
 func set_color_mode_by_time()->void:
 	var now = Time.get_datetime_dict_from_system()
 	if now["hour"] < 6 or now["hour"] >= 18 :
@@ -89,8 +130,14 @@ func update_color(darkmode :bool)->void:
 
 # change dark mode by time
 var old_time_dict = Time.get_datetime_dict_from_system() # datetime dict
+var old_minute_dict = Time.get_datetime_dict_from_system() # datetime dict
 func _on_timer_day_night_timeout() -> void:
 	var time_now_dict = Time.get_datetime_dict_from_system()
+
+	if old_minute_dict["minute"] != time_now_dict["minute"]:
+		$AniMove.start_with_step(1)
+		old_minute_dict = time_now_dict
+
 	if old_time_dict["hour"] != time_now_dict["hour"]:
 		old_time_dict = time_now_dict
 		match time_now_dict["hour"]:
