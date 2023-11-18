7e095ee87f7284541bbe471bfb8c04f94b16b57e
SeukWon Kang <kasworld@gmail.com>
2023-11-18 17:07:55 +0900

    color mode analog clock

--- analog_clock/analog_clock.gd
+++ analog_clock/analog_clock.gd
@@ -12,43 +12,60 @@ var dial_nums :Array
 func init(center :Vector2 , r :float, tz_s :float) -> void:
 	tz_shift = tz_s
 	clock_R = r
-
 	draw_dial(center, clock_R)
-#	draw_dial(-clock_R/2, 0,clock_R/4)
-#	draw_dial( clock_R/2, 0,clock_R/4)
-
-	HourHand = clock_hand(center, Global.colors.hour, 1.0/25, 0.7 )
-	add_child(HourHand)
-	HourHand2 = clock_hand(center, Global.colors.hour2, 1.0/100, 0.65)
-	add_child(HourHand2)
-	MinuteHand = clock_hand(center, Global.colors.minute, 1.0/50, 0.9)
-	add_child(MinuteHand)
-	SecondHand = clock_hand(center, Global.colors.second, 1.0/100, 1.0)
-	add_child(SecondHand)
-
+	draw_hand(center)
 	add_child(Global.new_circle_fill(center, clock_R/25, Global.colors.center_circle1))
 	add_child(Global.new_circle_fill(center, clock_R/30, Global.colors.center_circle2))
 
+var gr_hand_hour_1 :Gradient
+var gr_hand_hour_2 :Gradient
+var gr_hand_minute :Gradient
+var gr_hand_second :Gradient
+func draw_hand(center :Vector2)->void:
+	gr_hand_hour_1 = new_gradient(Global.colors.hour)
+	gr_hand_hour_2 = new_gradient(Global.colors.hour2)
+	gr_hand_minute = new_gradient(Global.colors.minute)
+	gr_hand_second = new_gradient(Global.colors.second)
+	HourHand = new_clock_hand(center, gr_hand_hour_1, 1.0/25, 0.7 )
+	add_child(HourHand)
+	HourHand2 = new_clock_hand(center, gr_hand_hour_2, 1.0/100, 0.65)
+	add_child(HourHand2)
+	MinuteHand = new_clock_hand(center, gr_hand_minute, 1.0/50, 0.9)
+	add_child(MinuteHand)
+	SecondHand = new_clock_hand(center, gr_hand_second, 1.0/100, 1.0)
+	add_child(SecondHand)
+
+
+var gr_360_1 :Gradient
+var gr_360_2 :Gradient
+var gr_90_1 :Gradient
+var gr_90_2 :Gradient
+var gr_30 :Gradient
+var gr_6 :Gradient
+var gr_1 :Gradient
 func draw_dial(p :Vector2, r :float):
 	var w = r/30
-#	add_child(Global.new_circle(p, r-w*0, Global.colors.outer_circle1, w/15))
-#	add_child(Global.new_circle(p, r-w*1, Global.colors.outer_circle2, w/15))
-#	add_child(Global.new_circle(p, r-w*2, Global.colors.outer_circle3, w/15))
-#	add_child(Global.new_circle(p, r-w*3, Global.colors.outer_circle4, w/15))
+	gr_360_1 = new_gradient(Global.colors.dial_360_1)
+	gr_360_2 = new_gradient(Global.colors.dial_360_2)
+	gr_90_1 = new_gradient(Global.colors.dial_90_1)
+	gr_90_2 = new_gradient(Global.colors.dial_90_2)
+	gr_30 = new_gradient(Global.colors.dial_30)
+	gr_6 = new_gradient(Global.colors.dial_6)
+	gr_1 = new_gradient(Global.colors.dial_1)
 	for i in range(0,360):
 		var rad = deg2rad(i)
 		if i == 0:
-			add_child( new_clock_face( p, r, Global.colors.dial_360_1, rad, r/50, w*3, 0 ) )
-			add_child( new_clock_face( p, r, Global.colors.dial_360_2, rad, r/200, w*3, 0 ) )
+			add_child( new_clock_face( p, r, gr_360_1, rad, r/50, w*3, 0 ) )
+			add_child( new_clock_face( p, r, gr_360_2, rad, r/200, w*3, 0 ) )
 		elif i % 90 ==0:
-			add_child( new_clock_face( p, r, Global.colors.dial_90_1, rad, r/100, w*3, 0 ) )
-			add_child( new_clock_face( p, r, Global.colors.dial_90_2, rad, r/300, w*2, 0 ) )
+			add_child( new_clock_face( p, r, gr_90_1, rad, r/100, w*3, 0 ) )
+			add_child( new_clock_face( p, r, gr_90_2, rad, r/300, w*2, 0 ) )
 		elif i % 30 == 0 :
-			add_child( new_clock_face( p, r, Global.colors.dial_30, rad, r/150, w*3, 0 ) )
+			add_child( new_clock_face( p, r, gr_30, rad, r/150, w*3, 0 ) )
 		elif i % 6 == 0 :
-			add_child( new_clock_face( p, r, Global.colors.dial_6, rad, r/200, w*2, 0 ) )
+			add_child( new_clock_face( p, r, gr_6, rad, r/200, w*2, 0 ) )
 		else :
-			add_child( new_clock_face( p, r, Global.colors.dial_1, rad, r/400, w*1, 0 ) )
+			add_child( new_clock_face( p, r, gr_1, rad, r/400, w*1, 0 ) )
 
 	for i in range(1,13):
 		var n = hour_letter(p,r, i)
@@ -58,6 +75,22 @@ func draw_dial(p :Vector2, r :float):
 func update_color()->void:
 	for n in dial_nums:
 		Global.set_label_color(n,Global.colors.dial_num[0], Global.colors.dial_num[1] )
+	gr_360_1.colors = Global.colors.dial_360_1
+	gr_360_2.colors = Global.colors.dial_360_2
+	gr_90_1.colors = Global.colors.dial_90_1
+	gr_90_2.colors = Global.colors.dial_90_2
+	gr_30.colors = Global.colors.dial_30
+	gr_6.colors = Global.colors.dial_6
+	gr_1.colors = Global.colors.dial_1
+	gr_hand_hour_1.colors = Global.colors.hour
+	gr_hand_hour_2.colors = Global.colors.hour2
+	gr_hand_minute.colors = Global.colors.minute
+	gr_hand_second.colors = Global.colors.second
+
+func new_gradient(co_list :Array)->Gradient:
+	var gr = Gradient.new()
+	gr.colors = co_list
+	return gr
 
 func hour_letter(p :Vector2,r :float,  i :int)->Label:
 	var lb = Label.new()
@@ -70,9 +103,7 @@ func hour_letter(p :Vector2,r :float,  i :int)->Label:
 	lb.position = Vector2( r*0.85 * cos(rad), r*0.85 * sin(rad) ) + p - Vector2(r*0.08,r*0.06)
 	return lb
 
-func new_clock_face(p :Vector2, r :float, co_list :Array, rad :float, w :float, h :float, y :float) -> Line2D:
-	var gr = Gradient.new()
-	gr.colors = co_list
+func new_clock_face(p :Vector2, r :float, gr :Gradient, rad :float, w :float, h :float, y :float) -> Line2D:
 	var cr = Line2D.new()
 	cr.gradient = gr
 	cr.width = w
@@ -81,9 +112,7 @@ func new_clock_face(p :Vector2, r :float, co_list :Array, rad :float, w :float, 
 	cr.position = p
 	return cr
 
-func clock_hand(p :Vector2, co_list :Array, w :float, h: float) -> Line2D:
-	var gr = Gradient.new()
-	gr.colors = co_list
+func new_clock_hand(p :Vector2, gr :Gradient, w :float, h: float) -> Line2D:
 	var cr = Line2D.new()
 	cr.gradient = gr
 	cr.width = w*clock_R
