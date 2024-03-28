fb29aa7a5003e6a3805cd673c234626ee1643fea
SeukWon Kang <kasworld@gmail.com>
2024-03-28 17:46:03 +0900

    add dial line align

--- analog_dial/analog_dial.gd
+++ analog_dial/analog_dial.gd
@@ -2,36 +2,46 @@ extends Node2D
 
 class_name AnalogDial
 
-var main_radius :float
+enum NumberType {Hour,Minute,Degree}
+enum LineAlign {In,Mid,Out}
+
 var dial_lines :PackedVector2Array =[]
+
+var main_radius :float
 var line_thick_rate :float
-var font_size_rate :float
+var line_align :LineAlign
+var nums_font_size_rate :float
 var nums_radius_rate :float
 var num_outline_w :int
+var number_type :NumberType
 
-enum DrawNumMode {Hour,Minute,Degree}
-var draw_num_mode :DrawNumMode
-
-func init(r:float, numrate:float = 0.9, ftrate :float = 0.09,numoutline :int = 4, lntk :float =0.004,  dnm :DrawNumMode = DrawNumMode.Hour)->void:
+func init(r:float, lntk :float = 0.004, lnan :LineAlign = LineAlign.In, numrate:float = 0.9, ftrate :float = 0.09, numoutline :int = 4, dnm :NumberType = NumberType.Hour)->void:
 	main_radius = r
-	nums_radius_rate = numrate
-	font_size_rate = ftrate
-	num_outline_w = numoutline
 	line_thick_rate = lntk
-	draw_num_mode = dnm
+	line_align = lnan
+	nums_radius_rate = numrate
+	nums_font_size_rate = ftrate
+	num_outline_w = numoutline
+	number_type = dnm
 	make_dial_lines(main_radius)
 
 func make_dial_lines(r :float)->void:
 	for i in range(0,360):
 		var rad = deg_to_rad(i+180)
-		var r1 :float
+		var offset :float = 0
 		if i % 30 == 0 :
-			r1 = r*0.94
+			offset = r*0.06
 		elif i % 6 == 0 :
-			r1 = r*0.96
+			offset = r*0.04
 		else :
-			r1 = r*0.98
-		dial_lines.append_array([ make_pos_by_rad_r(rad,r1),make_pos_by_rad_r(rad,r) ])
+			offset = r*0.02
+		match line_align:
+			LineAlign.In :
+				dial_lines.append_array([ make_pos_by_rad_r(rad,r-offset),make_pos_by_rad_r(rad,r) ])
+			LineAlign.Mid :
+				dial_lines.append_array([ make_pos_by_rad_r(rad,r-offset/2),make_pos_by_rad_r(rad,r+offset/2) ])
+			LineAlign.Out :
+				dial_lines.append_array([ make_pos_by_rad_r(rad,r),make_pos_by_rad_r(rad,r+offset) ])
 
 func make_pos_by_rad_r(rad:float, r :float)->Vector2:
 	return Vector2(sin(rad)*r, cos(rad)*r)
@@ -44,19 +54,19 @@ func _draw() -> void:
 	if w < 1 :
 		w = -1
 	draw_multiline(dial_lines,Global2d.colors.dial_1, w)
-	match draw_num_mode:
-		DrawNumMode.Hour:
+	match number_type:
+		NumberType.Hour:
 			for i in range(1,13):
 				var rad = deg_to_rad( -i*(360.0/12.0) -180)
-				draw_letter(rad,nums_radius_rate *main_radius, main_radius*font_size_rate, i)
-		DrawNumMode.Minute:
+				draw_letter(rad,nums_radius_rate *main_radius, main_radius*nums_font_size_rate, i)
+		NumberType.Minute:
 			for i in range(0,60,5):
 				var rad = deg_to_rad( -i*(360.0/60.0) -180)
-				draw_letter(rad,nums_radius_rate *main_radius, main_radius*font_size_rate, i)
-		DrawNumMode.Degree:
+				draw_letter(rad,nums_radius_rate *main_radius, main_radius*nums_font_size_rate, i)
+		NumberType.Degree:
 			for i in range(0,360,30):
 				var rad = deg_to_rad( -i*(360.0/360.0) -180)
-				draw_letter(rad,nums_radius_rate *main_radius, main_radius*font_size_rate, i)
+				draw_letter(rad,nums_radius_rate *main_radius, main_radius*nums_font_size_rate, i)
 
 func draw_letter(rad :float, r :float, fsize :float, i :int)->void:
 	var t = "%d" % i
