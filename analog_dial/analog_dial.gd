0465e4dbd97846508bb967ead39bfaa4f73a3c4b
SeukWon Kang <kasworld@gmail.com>
2024-03-28 13:49:22 +0900

    dial num outline

--- analog_dial/analog_dial.gd
+++ analog_dial/analog_dial.gd
@@ -7,14 +7,16 @@ var dial_lines :PackedVector2Array =[]
 var line_thick_rate :float
 var font_size_rate :float
 var nums_radius_rate :float
+var num_outline_w :int
 
 enum DrawNumMode {Hour,Minute,Degree}
 var draw_num_mode :DrawNumMode
 
-func init(r:float, numrate:float = 0.9, ftrate :float = 0.1, lntk :float =0.004,  dnm :DrawNumMode = DrawNumMode.Hour)->void:
+func init(r:float, numrate:float = 0.9, ftrate :float = 0.09,numoutline :int = 4, lntk :float =0.004,  dnm :DrawNumMode = DrawNumMode.Hour)->void:
 	main_radius = r
 	nums_radius_rate = numrate
 	font_size_rate = ftrate
+	num_outline_w = numoutline
 	line_thick_rate = lntk
 	draw_num_mode = dnm
 	make_dial_lines(main_radius)
@@ -45,34 +47,25 @@ func _draw() -> void:
 	match draw_num_mode:
 		DrawNumMode.Hour:
 			for i in range(1,13):
-				draw_hour_letter(nums_radius_rate *main_radius, main_radius*font_size_rate, i)
+				var rad = deg_to_rad( -i*(360.0/12.0) -180)
+				draw_letter(rad,nums_radius_rate *main_radius, main_radius*font_size_rate, i)
 		DrawNumMode.Minute:
 			for i in range(0,60,5):
-				draw_minute_letter(nums_radius_rate *main_radius, main_radius*font_size_rate, i)
+				var rad = deg_to_rad( -i*(360.0/60.0) -180)
+				draw_letter(rad,nums_radius_rate *main_radius, main_radius*font_size_rate, i)
 		DrawNumMode.Degree:
 			for i in range(0,360,30):
-				draw_degree_letter(nums_radius_rate *main_radius, main_radius*font_size_rate, i)
+				var rad = deg_to_rad( -i*(360.0/360.0) -180)
+				draw_letter(rad,nums_radius_rate *main_radius, main_radius*font_size_rate, i)
 
-func draw_hour_letter(r :float,fsize :float, i :int)->void:
-	var rad = deg_to_rad( -i*(360.0/12.0) -180)
+func draw_letter(rad :float, r :float, fsize :float, i :int)->void:
 	var t = "%d" % i
 	var pos = make_pos_by_rad_r(rad, r)
 	var offset = Vector2(-fsize/3.5*t.length(),fsize/3)
-	draw_string(Global2d.font, pos+offset, t, HORIZONTAL_ALIGNMENT_CENTER, -1,  fsize, Global2d.colors.dial_num )
-
-func draw_minute_letter(r :float, fsize :float,  i :int)->void:
-	var rad = deg_to_rad( -i*(360.0/60.0) -180)
-	var t = "%d" % i
-	var pos = make_pos_by_rad_r(rad, r)
-	var offset = Vector2(-fsize/3.5*t.length(),fsize/3)
-	draw_string(Global2d.font, pos+offset, t, HORIZONTAL_ALIGNMENT_CENTER, -1,  fsize, Global2d.colors.dial_num )
-
-func draw_degree_letter(r :float, fsize :float,  i :int)->void:
-	var rad = deg_to_rad( -i*(360.0/360.0) -180)
-	var t = "%d" % i
-	var pos = make_pos_by_rad_r(rad, r)
-	var offset = Vector2(-fsize/3.5*t.length(),fsize/3)
-	draw_string(Global2d.font, pos+offset, t, HORIZONTAL_ALIGNMENT_CENTER, -1,  fsize, Global2d.colors.dial_num )
+	if num_outline_w == 0:
+		draw_string(Global2d.font, pos+offset, t, HORIZONTAL_ALIGNMENT_CENTER, -1,  fsize, Global2d.colors.dial_num )
+	else:
+		draw_string_outline(Global2d.font, pos+offset, t, HORIZONTAL_ALIGNMENT_CENTER, -1,  fsize,num_outline_w, Global2d.colors.dial_num )
 
 func draw_cross(p :Vector2, l:float, co :Color)->void:
 	draw_line(p + Vector2(-l/2,0),p + Vector2(l/2,0), co,-1 )
