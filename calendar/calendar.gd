cd71fd12450da780dd77be7cd303106fd9a7a58f
seukwonkang <kasworld@gmail.com>
2026-04-11 17:37:21 +0900

    code arrange

--- calendar/calendar.gd
+++ calendar/calendar.gd
@@ -30,13 +30,12 @@ func draw_calendar(unix_time :int) -> void:
 		var week := 1
 		var co :Color = Global2d.colors.weekday[wd]
 		var fsize := fh*0.9
-		var today_adj := Vector2(0,0)
 		var text :String = Global2d.weekdaystring[wd]
+		var pos := Vector2(fw*wd,fh*week) - csize/2 +offset+Vector2(fh/10 ,0)
 		if wd == datetime_dict["weekday"] :
 			co = Global2d.colors.today
 			fsize = fh *1.2
-			today_adj = Vector2(-fsize*0.1, fsize*0.08)
-		var pos := Vector2(fw*wd,fh*week) - csize/2 +offset+Vector2(fh/10 ,0) + today_adj
+			pos += Vector2(-fsize*0.1, fsize*0.08)
 		draw_string(Global2d.font, pos , text, HORIZONTAL_ALIGNMENT_CENTER, -1, fsize as int, co )
 
 	var day_index :int = unix_time - (7 + datetime_dict["weekday"] )*24*60*60
@@ -45,15 +44,14 @@ func draw_calendar(unix_time :int) -> void:
 			var day_index_dict := Time.get_date_dict_from_unix_time(day_index)
 			var co :Color = Global2d.colors.weekday[wd]
 			var fsize := fh*0.9
-			var today_adj := Vector2(0,0)
 			var text := "%2d" % day_index_dict["day"]
+			var pos := Vector2(fw*wd,fh*week) - csize/2 +offset
 			if day_index_dict["month"] != datetime_dict["month"]:
 				co = Global2d.make_shadow_color(co)
 			elif day_index_dict["day"] == datetime_dict["day"]:
 				co = Global2d.colors.today
 				fsize = fh*1.2
-				today_adj = Vector2(-fsize*0.08 * text.length(), fsize*0.08)
-			var pos := Vector2(fw*wd,fh*week) - csize/2 +offset + today_adj
+				pos += Vector2(-fsize*0.08 * text.length(), fsize*0.08)
 			draw_string(Global2d.font, pos , text, HORIZONTAL_ALIGNMENT_CENTER, -1, fsize as int, co )
 			day_index += 24*60*60
 
@@ -64,7 +62,6 @@ func _draw() -> void:
 var old_time_dict := {"day":0} # datetime dict
 func _on_timer_timeout() -> void:
 	var time_now_dict := Time.get_datetime_dict_from_system()
-
 	# date changed, update datelabel, calendar
 	if old_time_dict["day"] != time_now_dict["day"]:
 		old_time_dict = time_now_dict
